b3571715f58eab4cb37518d68095ede7689a9ca9
sjyoti <soumyojyoti.pal@honeywell.com>
2022-05-06 23:07:40 +0200

    created first get endpoints

---
+++ rest-api/index.js
@@ -0,0 +1,32 @@
+const app = require("express")();
+
+/*On command >node index.js, the app starts listening for incoming connections 
+at localhost:3000*/ 
+app.listen(
+    3000,
+    () => console.log('Node app is running!') 
+);
+
+/*get takes parameters:
+> path - the path of the endpoint
+> callback function, which takes parameters
+>> request - request from the client
+>> response - response to be sent to the client
+
+req.status takes the parameter of the status code to be sent
+send() takes the json payload as the response to be sent*/
+
+/*return user details on hitting localhst:3000/users */
+app.get('/users', (req,res) => {
+    res.status(200).send([
+        {name: 'John', id: 1},
+        {name: 'Cena', id: 2},
+    ]);
+});
+
+/*return user detail of user 3 on hitting localhst:3000/user/3 */
+app.get('/user/3', (req,res) => {
+    res.status(200).send([
+        {user: 'Goldman', id: 3},
+    ]);
+});
